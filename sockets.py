375c02505aa9c9d2cb041a62c5f22aada4daa3cf
curopium <curnow@ualberta.ca>
2015-03-14 13:47:51 -0600

    got server all implimented?

--- sockets.py
+++ sockets.py
@@ -14,17 +14,29 @@
 # limitations under the License.
 #
 import flask
-from flask import Flask, request
+from flask import Flask, request, redirect
 from flask_sockets import Sockets
 import gevent
 from gevent import queue
 import time
 import json
 import os
+import json
 
 app = Flask(__name__)
 sockets = Sockets(app)
 app.debug = True
+clients = list()
+
+class Client:
+    def __init__(self):
+        self.queue = queue.Queue()
+
+    def put(self, v):
+        self.queue.put_nowait(v)
+
+    def get(self):
+        return self.queue.get()
 
 class World:
     def __init__(self):
@@ -61,6 +73,7 @@ class World:
 
 myWorld = World()        
 
+#TODO?
 def set_listener( entity, data ):
     ''' do something with the update ! '''
 
@@ -69,19 +82,51 @@ myWorld.add_set_listener( set_listener )
 @app.route('/')
 def hello():
     '''Return something coherent here.. perhaps redirect to /static/index.html '''
-    return None
+    return redirect('static/index.html')
+
+def send_all(msg):
+    for client in clients:
+        client.put( msg)
+
+def send_all_jason(obj):
+    send_all( json.dumps(obj))
 
 def read_ws(ws,client):
     '''A greenlet function that reads from the websocket and updates the world'''
     # XXX: TODO IMPLEMENT ME
-    return None
+    #code used from in class notes
+    try:
+        while True:
+            msg = ws.receieve()
+            print "WebSocket Received: %s" % msg
+            if (msg is not None):
+                packet = json.loads(msg)
+                send_all_json(packet)
+            else:
+                break
+    except:
+        '''Done'''
 
 @sockets.route('/subscribe')
 def subscribe_socket(ws):
     '''Fufill the websocket URL of /subscribe, every update notify the
        websocket and read updates from the websocket '''
     # XXX: TODO IMPLEMENT ME
-    return None
+    # code also used from in-class notes
+    client = Client()
+    clients.append(client)
+    g = gevent.spawn( read_ws, ws, client )
+    print "Trying to subscribe socket..."
+    try:
+        while True:
+            msg = client.get()
+            print "Got message"
+            ws.send(msg)
+    except Exception as e:
+        print "WS Error %s" % e
+    finally:
+        clients.remove(client)
+        gevent.kill(g)
 
 
 def flask_post_json():
@@ -97,23 +142,26 @@ def flask_post_json():
 @app.route("/entity/<entity>", methods=['POST','PUT'])
 def update(entity):
     '''update the entities via this interface'''
-    return None
+    json_data = flask_post_json()
+    myWorld.set(entity, json_data)
+    return json.dumps(myWorld.get(entity))
 
 @app.route("/world", methods=['POST','GET'])    
 def world():
     '''you should probably return the world here'''
-    return None
+    return json.dumps(myWorld.world())
 
 @app.route("/entity/<entity>")    
 def get_entity(entity):
     '''This is the GET version of the entity interface, return a representation of the entity'''
-    return None
+    return json.dumps(myWorld.get(entity))
 
 
 @app.route("/clear", methods=['POST','GET'])
 def clear():
     '''Clear the world out!'''
-    return None
+    myWorld.clear()
+    return json.dumps(myWorld.world())
 
 
 
